2b45257bf4a3bbb48aeeb1c69f426b91f2d96263
LynanThon <thonlynan17@kit.edu.kh>
2018-06-26 19:18:10 +0700

    student name id function

---
+++ Practice05.scala
@@ -0,0 +1,9 @@
+class student
+{
+    def info(name: String,id: Int)
+    {
+        println("Student's name is: " + name + "\nStudent's ID is: " + id);
+    }
+}
+var s = new student();
+s.info("Lynan",123);
